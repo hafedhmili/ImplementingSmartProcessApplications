35306838c6346a59b37846dc311ba0823a156ee0
hafedhmili <mili.hafedh@uqam.ca>
2026-07-03 21:57:51 -0400

    got the file upload sample to work, after fixing version numbers for io.netty and I don't remember what else. I also regenerated keys for connecting, but I am no longer able to connect. It times out, and it appears to not be allowed to connect ('Not Authorized to Connect')

--- PatientMonitoring/heartmonitor/code/heartmonitoringdevicedriver/src/main/java/spa/samples/heartpatientmonitoring/domain/impl/device/driver/HeartMonitoringDeviceDriverImpl.java
+++ PatientMonitoring/heartmonitor/code/heartmonitoringdevicedriver/src/main/java/spa/samples/heartpatientmonitoring/domain/impl/device/driver/HeartMonitoringDeviceDriverImpl.java
@@ -116,7 +116,8 @@ public class HeartMonitoringDeviceDriverImpl implements HeartMonitoringDeviceDri
         File file = new File(ecgFile.getFilePath(), ecgFile.getFileName());
         // get parameters for uploading the ECG file to the IoT Hub
         // The blob name on IoT hub will be deviceID_patientID_ECGstartTime
-        String blobName = heartMonitorDevice.getDeviceID()+ "_"+ heartMonitorDevice.getPatientID() + "_" + ecg.getStartTime().getEpochSecond();
+//        String blobName = heartMonitorDevice.getDeviceID()+ "_"+ heartMonitorDevice.getPatientID() + "_" + ecg.getStartTime().getEpochSecond();
+        String blobName = heartMonitorDevice.getPatientID() ;
         FileUploadSasUriRequest request = new FileUploadSasUriRequest(blobName);
         FileUploadSasUriResponse sasUriResponse = deviceClient.getFileUploadSasUri(request);
 
